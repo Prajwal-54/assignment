878c8053d616a4753224c9ed2f84d6fc441e3e0f
prajwal-54 <62026455+Prajwal-54@users.noreply.github.com>
2022-10-22 20:32:54 +0530

    question2 solution

---
+++ question2.js
@@ -0,0 +1,71 @@
+const menu = {
+  espresso: { milk: 60, cream: 75, latte: 100 },
+  cappuccino: { milk: 80, cream: 90, latte: 125 },
+  latte: { milk: 100, cream: 125, latte: 150 },
+};
+
+class Coffee {
+  constructor(name, addOns) {
+    this.name = name;
+    this.addOns = addOns;
+  }
+  getName() {
+    return this.name;
+  }
+  getAddOns() {
+    return this.addOns;
+  }
+}
+
+class Order {
+  constructor() {
+    this.coffees = []; //stores coffee objects
+    this.orderCost = 0;
+  }
+  addCoffee(cofee) {
+    this.coffees.push(cofee);
+  }
+  getTotalAmount() {
+    //returns coffee cost from stored coffee objects
+    let amount = 0;
+    try {
+      this.coffees.forEach((coffee) => {
+        let name = coffee.name;
+        let addOns = coffee.addOns;
+
+        addOns.forEach((addOn) => {
+          amount += menu[name][addOn];
+        });
+      });
+      this.orderCost = amount;
+      return this.orderCost;
+    } catch (e) {
+      return this.orderCost;
+    }
+  }
+}
+
+window.onload = (event) => {
+  const order = new Order();
+  let orderComplete = true;
+  while (orderComplete) {
+    let name = prompt(
+      "coffees available are : \n1)Espresso\n2)Cappuccino\n3)Latte\nEnter name coffee you want to buy :",
+      "latte"
+    ).toLowerCase();
+    menu;
+    let addon = prompt(
+      "addOns available are : \n1)Milk\n2)Cream\n3)Latte\nEnter name AddOns you want to add \nspace separated names ",
+      "milk latte"
+    ).toLowerCase();
+
+    //create coffee from input
+    let addOns = addon.split(" ");
+    let coffee = new Coffee(name, addOns);
+    order.addCoffee(coffee);
+
+    orderComplete = confirm("do you want to buy more coffee?");
+  }
+
+  alert("Order Amount is : " + order.getTotalAmount());
+};
